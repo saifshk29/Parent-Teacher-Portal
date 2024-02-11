cc1c17e589b9297b57801171980b8e324c703107
saifshk29 <saifshk29@gmail.com>
2024-02-11 08:24:01 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       'hero-pattern': "url('images\rm314-adj-10.jpg')",
