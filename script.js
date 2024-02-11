51cf68655e6ab66903cffac1cc38bc92b35acb87
saifshk29 <saifshk29@gmail.com>
2024-02-11 07:49:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayInput() {
+    // Get the input value
+    var inputText = document.getElementsByClassName("stdprn").value;
+  
+    // Display the input value in the Dagboard section
+    var var_123 = document.getElementById("usernameDisplay");
+    
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      'hero-pattern': "url('images\rm314-adj-10.jpg')",
+      
+    },
+  },
+  plugins: [],
+}
+
