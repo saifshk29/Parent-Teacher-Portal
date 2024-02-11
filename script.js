d4a6086cb7a1a33b4a8a517c3d3a5f55a4e0d36c
saifshk29 <saifshk29@gmail.com>
2024-02-11 09:27:58 +0530

    alert

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function displayInput() {
-    // Get the input value
-    var inputText = document.getElementsByClassName("stdprn").value;
-  
-    // Display the input value in the Dagboard section
-    var var_123 = document.getElementById("usernameDisplay");
-    
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    const scheduling = document.getElementById('scheduling');
+
+    scheduling.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        
+        var date_meeting = document.getElementById("start").value;
+        var time_start = document.getElementById("meetingtime").value;
+
+        var selectedTime = new Date("1970-01-01T" + time_start);
+        var hours = selectedTime.getHours();
+        var minutes = selectedTime.getMinutes();
+
+       
+        if (hours >= 15 && hours < 17) {
+            
+            alert(`Hello, your meeting is booked for ${date_meeting} at ${time_start}`);
+        } else {
+            
+            alert("Please select a time between 3 PM and 5 PM.");
+        }
+    });
+});
